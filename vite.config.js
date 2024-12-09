49d1c17da97f8e1f32a4c04009c2e641d89356c0
Stéphan Mestach <smestach@bluesquare.org>
2024-12-09 20:32:00 +0100

    align with existing repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/oh-reports-ssc/",
+  base: "/poc-oh-github-pages/",
   plugins: [react()],
 })
